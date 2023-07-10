71aa3ca8db2e46dc8425f7cca72ad3443e7bbb77
sqxsss <1514713140@qq.com>
2023-07-10 18:53:53 -0400

    update PaymentService doPayment Api

--- src/main/java/net/nvsoftware/iPaymentService/IPaymentServiceApplication.java
+++ src/main/java/net/nvsoftware/iPaymentService/IPaymentServiceApplication.java
@@ -2,6 +2,7 @@ package net.nvsoftware.iPaymentService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 public class IPaymentServiceApplication {
