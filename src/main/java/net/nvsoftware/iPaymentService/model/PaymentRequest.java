3b6f423a8636074f0e159098dce5c7ef4b240baf
sqxsss <1514713140@qq.com>
2023-07-10 18:27:34 -0400

    PaymentService doPayment Api

---
+++ src/main/java/net/nvsoftware/iPaymentService/model/PaymentRequest.java
@@ -0,0 +1,24 @@
+package net.nvsoftware.iPaymentService.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PaymentRequest {
+
+    private long orderId;
+
+    private PaymentMode paymentMode;
+
+    private String referenceNumber;
+
+    private long totalAmount;
+
+}
--- src/main/java/net/nvsoftware/iPaymentService/service/PaymentService.java
+++ src/main/java/net/nvsoftware/iPaymentService/service/PaymentService.java
@@ -1,6 +1,9 @@
 package net.nvsoftware.iPaymentService.service;
 
+import net.nvsoftware.iPaymentService.model.PaymentRequest;
+
 public interface PaymentService {
 
 
+    long doPayment(PaymentRequest paymentRequest);
 }
